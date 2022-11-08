0a017e46b75d31dbac3c447f219c1ae5c79dea43
Jordi Izquierdo <hi@izquiratops.dev>
2022-11-08 20:51:23 +0100

    Favicon + Nicer builder assets report + requestPointerLock()

--- build.js
+++ build.js
@@ -2,7 +2,24 @@ const fs = require('fs');
 const path = require('path');
 const esbuild = require('esbuild');
 
-const copyRecursiveSync = (src, dest) => {
+function formatSize(sizeInBytes) {
+    if (sizeInBytes > 1024 * 1024) {
+        return `${(sizeInBytes / 1024 * 1024).toFixed(3)}Mb`;
+    } else if (sizeInBytes > 1024) {
+        return `${(sizeInBytes / 1024).toFixed(3)}kb`;
+    } else {
+        return `${sizeInBytes}b`
+    }
+}
+
+function copyFile(src, dest) {
+    const { size } = fs.statSync(src);
+    console.log(`${src}\t\t${formatSize(size)}`);
+
+    fs.copyFileSync(src, dest);
+}
+
+function copyRecursiveSync(src, dest) {
     const isDirectory = fs.existsSync(src) && fs.statSync(src).isDirectory();
 
     if (isDirectory) {
@@ -17,10 +34,10 @@ const copyRecursiveSync = (src, dest) => {
         });
     } else {
         if (path.basename(src) !== '.gitkeep') {
-            fs.copyFileSync(src, dest);
+            copyFile(src, dest);
         }
     }
-};
+}
 
 (() => {
     const result = esbuild.buildSync({
@@ -35,6 +52,7 @@ const copyRecursiveSync = (src, dest) => {
     console.log('ESBuild report', esbuild.analyzeMetafileSync(result.metafile));
 
     copyRecursiveSync('./src/assets', './dist/assets');
+
     fs.copyFileSync('./src/index.html', './dist/index.html');
     fs.copyFileSync('./src/style.css', './dist/style.css');
 })();
